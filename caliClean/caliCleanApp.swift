c3a950e0e7cb89eff65f5e4ce5629ccf77269bce
Ilayda Koker <ilayda.n.koker@gmail.com>
2023-07-18 13:34:57 -0700

    Initial Commit

---
+++ caliClean/caliCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  caliCleanApp.swift
+//  caliClean
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct caliCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
